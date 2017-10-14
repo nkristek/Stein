9f72ba666898d2ba83bbbda4fdbadbf2626e3c2c
Niclas Kristek <nkristek@users.noreply.github.com>
2017-10-14 20:14:43 +0200

    Change the location of the loading label

---
+++ Stein/Converters/AnyBoolToVisibilityConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Stein.Converters
+{
+    public class AnyBoolToVisibilityConverter
+        : IMultiValueConverter
+    {
+        public static AnyBoolToVisibilityConverter Instance = new AnyBoolToVisibilityConverter();
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch (parameter as string)
+            {
+                case "Hidden": return values.Any(v => v is bool && (bool)v) ? Visibility.Hidden : Visibility.Collapsed;
+                default: return values.Any(v => v is bool && (bool)v) ? Visibility.Visible : Visibility.Collapsed;
+            }
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
