89e8291abc2d320819bf141d2fe1e8e5f1db4c18
Niclas Kristek <nkristek@users.noreply.github.com>
2017-11-19 22:55:56 +0100

    Fix icons and some minor ui improvements

--- Stein/App.xaml.cs
+++ Stein/App.xaml.cs
@@ -1,10 +1,6 @@
 ﻿using Stein.Configuration;
 using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
 using System.IO;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
