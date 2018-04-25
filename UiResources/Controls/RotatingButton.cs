e12c6bb0425c6fb63168332ca6bbc867003a517b
Niclas Kristek <nkristek@users.noreply.github.com>
2018-04-26 00:40:30 +0200

    Implement RotatingButton to animate the refresh button

---
+++ UiResources/Controls/RotatingButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace BenRuehl.UiResources.Controls
+{
+    public class RotatingButton
+        : Button
+    {
+        static RotatingButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(RotatingButton), new FrameworkPropertyMetadata(typeof(RotatingButton)));
+        }
+
+        public static readonly DependencyProperty IsRotatingProperty = DependencyProperty.Register(nameof(IsRotating), typeof(bool), typeof(RotatingButton), new PropertyMetadata(null));
+
+        public bool IsRotating
+        {
+            get => (bool)GetValue(IsRotatingProperty);
+            set => SetValue(IsRotatingProperty, value);
+        }
+    }
+}
