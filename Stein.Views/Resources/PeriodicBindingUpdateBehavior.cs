e74748a0bd2ac072749424db433cd9bee3cbe0ad
Niclas Kristek <nkristek@users.noreply.github.com>
2019-04-04 19:42:18 +0200

    Move helper classes for PeriodicBindingUpdateBehaviour into separate files

--- Stein.Views/Resources/PeriodicBindingUpdateBehavior.cs
+++ Stein.Views/Resources/PeriodicBindingUpdateBehavior.cs
@@ -26,9 +26,12 @@ namespace Stein.Views.Resources
 
         protected override void OnAttached()
         {
-            if (Interval == null) throw new ArgumentNullException(nameof(Interval));
-            if (Property == null) throw new ArgumentNullException(nameof(Property));
-            //Save a reference to the callback of the timer so this object will keep the timer alive but not vice versa.
+            if (Interval == null)
+                throw new ArgumentNullException(nameof(Interval));
+            if (Property == null)
+                throw new ArgumentNullException(nameof(Property));
+
+            // Save a reference to the callback of the timer so this object will keep the timer alive but not vice versa.
             _timerCallback = s =>
             {
                 try
@@ -52,8 +55,7 @@ namespace Stein.Views.Resources
 
         protected override void OnDetaching()
         {
-            _timer.Dispose();
-            _timerCallback = null;
+            Dispose();
             base.OnDetaching();
         }
 
@@ -61,90 +63,7 @@ namespace Stein.Views.Resources
         public void Dispose()
         {
             _timer?.Dispose();
-        }
-    }
-
-    internal enum PeriodicBindingUpdateMode
-    {
-        UpdateTarget, UpdateSource
-    }
-
-    /// <summary>
-    /// Wraps up a <see cref="System.Threading.Timer"/> with only a <see cref="WeakReference"/> to the callback so that the timer does not prevent GC from collecting the object that uses this timer.
-    /// Your object must hold a reference to the callback passed into this timer.
-    /// </summary>
-    internal class WeakTimer : IDisposable
-    {
-        private readonly Timer _timer;
-
-        private readonly WeakReference<TimerCallback> _weakCallback;
-
-        public WeakTimer(TimerCallback callback)
-        {
-            _timer = new Timer(OnTimerCallback);
-            _weakCallback = new WeakReference<TimerCallback>(callback);
-        }
-
-        public WeakTimer(TimerCallback callback, object state, int dueTime, int period)
-        {
-            _timer = new Timer(OnTimerCallback, state, dueTime, period);
-            _weakCallback = new WeakReference<TimerCallback>(callback);
-        }
-
-        public WeakTimer(TimerCallback callback, object state, TimeSpan dueTime, TimeSpan period)
-        {
-            _timer = new Timer(OnTimerCallback, state, dueTime, period);
-            _weakCallback = new WeakReference<TimerCallback>(callback);
-        }
-
-        public WeakTimer(TimerCallback callback, object state, uint dueTime, uint period)
-        {
-            _timer = new Timer(OnTimerCallback, state, dueTime, period);
-            _weakCallback = new WeakReference<TimerCallback>(callback);
-        }
-
-        public WeakTimer(TimerCallback callback, object state, long dueTime, long period)
-        {
-            _timer = new Timer(OnTimerCallback, state, dueTime, period);
-            _weakCallback = new WeakReference<TimerCallback>(callback);
-        }
-
-        private void OnTimerCallback(object state)
-        {
-            if (_weakCallback.TryGetTarget(out TimerCallback callback))
-                callback(state);
-            else
-                _timer.Dispose();
-        }
-
-        public bool Change(int dueTime, int period)
-        {
-            return _timer.Change(dueTime, period);
-        }
-
-        public bool Change(TimeSpan dueTime, TimeSpan period)
-        {
-            return _timer.Change(dueTime, period);
-        }
-
-        public bool Change(uint dueTime, uint period)
-        {
-            return _timer.Change(dueTime, period);
-        }
-
-        public bool Change(long dueTime, long period)
-        {
-            return _timer.Change(dueTime, period);
-        }
-
-        public bool Dispose(WaitHandle notifyObject)
-        {
-            return _timer.Dispose(notifyObject);
-        }
-
-        public void Dispose()
-        {
-            _timer.Dispose();
+            _timerCallback = null;
         }
     }
 }
---
+++ Stein.Views/Resources/WeakTimer.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Stein.Views.Resources
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// Wraps up a <see cref="T:System.Threading.Timer" /> with only a <see cref="T:System.WeakReference" /> to the callback so that the timer does not prevent GC from collecting the object that uses this timer.
+    /// Your object must hold a reference to the callback passed into this timer.
+    /// </summary>
+    internal class WeakTimer : IDisposable
+    {
+        private readonly Timer _timer;
+
+        private readonly WeakReference<TimerCallback> _weakCallback;
+
+        public WeakTimer(TimerCallback callback)
+        {
+            _timer = new Timer(OnTimerCallback);
+            _weakCallback = new WeakReference<TimerCallback>(callback);
+        }
+
+        public WeakTimer(TimerCallback callback, object state, int dueTime, int period)
+        {
+            _timer = new Timer(OnTimerCallback, state, dueTime, period);
+            _weakCallback = new WeakReference<TimerCallback>(callback);
+        }
+
+        public WeakTimer(TimerCallback callback, object state, TimeSpan dueTime, TimeSpan period)
+        {
+            _timer = new Timer(OnTimerCallback, state, dueTime, period);
+            _weakCallback = new WeakReference<TimerCallback>(callback);
+        }
+
+        public WeakTimer(TimerCallback callback, object state, uint dueTime, uint period)
+        {
+            _timer = new Timer(OnTimerCallback, state, dueTime, period);
+            _weakCallback = new WeakReference<TimerCallback>(callback);
+        }
+
+        public WeakTimer(TimerCallback callback, object state, long dueTime, long period)
+        {
+            _timer = new Timer(OnTimerCallback, state, dueTime, period);
+            _weakCallback = new WeakReference<TimerCallback>(callback);
+        }
+
+        private void OnTimerCallback(object state)
+        {
+            if (_weakCallback.TryGetTarget(out var callback))
+                callback(state);
+            else
+                _timer.Dispose();
+        }
+
+        public bool Change(int dueTime, int period)
+        {
+            return _timer.Change(dueTime, period);
+        }
+
+        public bool Change(TimeSpan dueTime, TimeSpan period)
+        {
+            return _timer.Change(dueTime, period);
+        }
+
+        public bool Change(uint dueTime, uint period)
+        {
+            return _timer.Change(dueTime, period);
+        }
+
+        public bool Change(long dueTime, long period)
+        {
+            return _timer.Change(dueTime, period);
+        }
+
+        public bool Dispose(WaitHandle notifyObject)
+        {
+            return _timer.Dispose(notifyObject);
+        }
+
+        public void Dispose()
+        {
+            _timer.Dispose();
+        }
+    }
+}
