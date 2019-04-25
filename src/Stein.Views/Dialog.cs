5c689967f3194a5122b4dc21aca96b4700d677c0
Niclas Kristek <nkristek@users.noreply.github.com>
2019-04-25 22:56:29 +0200

    Fix validation error adorner in dialogs

--- src/Stein.Views/Dialog.cs
+++ src/Stein.Views/Dialog.cs
@@ -1,19 +1,12 @@
 ﻿using System.Windows;
-using System.Windows.Controls;
-using AdonisUI;
-using AdonisUI.Extensions;
-using NKristek.Wpf.Converters;
 
 namespace Stein.Views
 {
     public abstract class Dialog : Window
     {
-        protected Dialog()
+        static Dialog()
         {
-            InitializeStyle();
-
-            //Style = TryFindResource(typeof(Dialog)) as Style;
-            //DefaultStyleKeyProperty.OverrideMetadata(typeof(Dialog), new FrameworkPropertyMetadata(typeof(Dialog)));
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Dialog), new FrameworkPropertyMetadata(typeof(Dialog)));
         }
         
         public object DialogButtons
@@ -22,49 +15,6 @@ namespace Stein.Views
             set => SetValue(DialogButtonsProperty, value);
         }
 
-        public static readonly DependencyProperty DialogButtonsProperty = DependencyProperty.Register("DialogButtons", typeof(object), typeof(Dialog), new PropertyMetadata(null));
-
-        private void InitializeStyle()
-        {
-            var template = new ControlTemplate(typeof(Dialog));
-
-            var grid = new FrameworkElementFactory(typeof(Grid));
-            grid.SetResourceReference(Grid.BackgroundProperty, Brushes.Layer0BackgroundBrush);
-            var firstRowDefinition = new FrameworkElementFactory(typeof(RowDefinition));
-            firstRowDefinition.SetValue(RowDefinition.HeightProperty, new GridLength(1.0, GridUnitType.Star));
-            grid.AppendChild(firstRowDefinition);
-            var secondRowDefinition = new FrameworkElementFactory(typeof(RowDefinition));
-            secondRowDefinition.SetValue(RowDefinition.HeightProperty, GridLength.Auto);
-            grid.AppendChild(secondRowDefinition);
-            template.VisualTree = grid;
-
-            // Dialog content
-
-            var contentPresenter = new FrameworkElementFactory(typeof(ContentPresenter));
-            contentPresenter.SetValue(Grid.RowProperty, 0);
-            contentPresenter.SetValue(ContentPresenter.ContentProperty, new TemplateBindingExtension(ContentProperty));
-            grid.AppendChild(contentPresenter);
-
-            // Dialog buttons
-
-            var buttonsGrid = new FrameworkElementFactory(typeof(Grid));
-            buttonsGrid.SetValue(Grid.RowProperty, 1);
-            buttonsGrid.SetResourceReference(Grid.BackgroundProperty, Brushes.Layer1BackgroundBrush);
-            buttonsGrid.SetValue(Grid.VisibilityProperty, new TemplateBindingExtension(DialogButtonsProperty)
-            {
-                Converter = ValueNullToInverseVisibilityConverter.Instance
-            });
-            buttonsGrid.SetValue(LayerExtension.LayerProperty, 1);
-            grid.AppendChild(buttonsGrid);
-
-            var buttonsPresenter = new FrameworkElementFactory(typeof(ContentPresenter));
-            buttonsPresenter.SetValue(ContentPresenter.ContentProperty, new TemplateBindingExtension(DialogButtonsProperty));
-            buttonsGrid.AppendChild(buttonsPresenter);
-
-            var style = new Style(typeof(Dialog), TryFindResource(typeof(Window)) as Style);
-            var templateSetter = new Setter(TemplateProperty, template);
-            style.Setters.Add(templateSetter);
-            Style = style;
-        }
+        public static readonly DependencyProperty DialogButtonsProperty = DependencyProperty.Register(nameof(DialogButtons), typeof(object), typeof(Dialog), new PropertyMetadata(null));
     }
 }
--- src/Stein.Views/Properties/AssemblyInfo.cs
+++ src/Stein.Views/Properties/AssemblyInfo.cs
@@ -21,6 +21,15 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("01aac7ae-9bf5-4b7e-9cce-0bac08848930")]
 
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+    //(used if a resource is not found in the page,
+    // or application resource dictionaries)
+    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+    //(used if a resource is not found in the page,
+    // app, or any theme specific resource dictionaries)
+)]
+
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
