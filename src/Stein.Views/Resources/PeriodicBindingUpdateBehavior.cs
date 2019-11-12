573d260e133b8b5b1cdb6375669c332550ed11c1
Niclas Kristek <nkristek@users.noreply.github.com>
2019-11-12 12:23:35 +0100

    Improve boundary checks

--- src/Stein.Views/Resources/PeriodicBindingUpdateBehavior.cs
+++ src/Stein.Views/Resources/PeriodicBindingUpdateBehavior.cs
@@ -26,8 +26,10 @@ namespace Stein.Views.Resources
 
         protected override void OnAttached()
         {
-            if (Interval == null)
+            if (Interval == default)
                 throw new ArgumentNullException(nameof(Interval));
+            if (Interval < TimeSpan.Zero)
+                throw new ArgumentException("Negative intervals are not supported.", nameof(Interval));
             if (Property == null)
                 throw new ArgumentNullException(nameof(Property));
 
